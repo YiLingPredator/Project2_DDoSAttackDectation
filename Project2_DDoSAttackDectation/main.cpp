08c2f87baab93ec5af3bb77f5567e88964e9bd83
YiLingPredator <lingyi0530@gmail.com>
2014-09-20 20:23:07 -0500

    Initial Commit

---
+++ Project2_DDoSAttackDectation/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Project2_DDoSAttackDectation
+//
+//  Created by YI LING on 9/20/14.
+//  Copyright (c) 2014 YI LING. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
